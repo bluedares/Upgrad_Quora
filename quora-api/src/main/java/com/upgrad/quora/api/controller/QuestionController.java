9972c4f1714241f4b2fc4ac5343775eb3ce89070
Venkatesh Elangovan <vikneshwar.e@ET-C02YQEBWLVCF.domain.name>
2020-12-02 11:51:22 +0530

    question controller comments

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -42,6 +42,18 @@ public class QuestionController {
     @Autowired
     private QuestionDeleteResponseTransformer questionDeleteResponseTransformer;
 
+    /**
+     *
+     * @param questionRequest
+     * @param accessToken
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws AuthenticationFailedException
+     * This api creates a new question with the question content shared via questionRequest and accessToken.
+     * QuestionRequestTransformer creates a temporary question object from questionRequest.
+     * Validate the access token before creating question. If any validations fail, one of the above exceptions are thrown.
+     * QuestionResponseTransformer creates question response from the created question entity.
+     */
     @RequestMapping(method = RequestMethod.POST, path = "/question/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionResponse> createQuestion(final QuestionRequest questionRequest, @RequestHeader("accessToken") final String accessToken) throws AuthorizationFailedException, AuthenticationFailedException {
         String bearerToken = tokenService.getBearerToken(accessToken);
@@ -50,6 +62,19 @@ public class QuestionController {
         QuestionResponse questionResponse = createQuestionResponseTransformer.transform(questionEntity);
         return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
     }
+
+    /**
+     *
+     * @param accessToken
+     * @return
+     * @throws AuthenticationFailedException
+     * @throws AuthorizationFailedException
+     * @throws QuestionNotFoundException
+     * This api fetches all the questions created in the application so far.
+     * Access token validations are performed. If any validations fail, one of the above exceptions are thrown.
+     * If no questions are found, appropriate error message is displayed.
+     * QuestionDetailsResponseTransformer creates response object from the list of question entities fetched from DB.
+     */
     @RequestMapping(method = RequestMethod.GET, path = "/question/all", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<List<QuestionDetailsResponse>> getAllQuestions(@RequestHeader("accessToken") final String accessToken) throws AuthenticationFailedException,AuthorizationFailedException, QuestionNotFoundException {
         String bearerToken = tokenService.getBearerToken(accessToken);
@@ -57,6 +82,21 @@ public class QuestionController {
         List<QuestionDetailsResponse> questionDetailsResponses = questionDetailsResponseTransformer.transform(questionEntities);
         return new ResponseEntity<List<QuestionDetailsResponse>>(questionDetailsResponses,HttpStatus.OK);
     }
+
+    /**
+     *
+     * @param userId
+     * @param accessToken
+     * @return
+     * @throws QuestionNotFoundException
+     * @throws AuthenticationFailedException
+     * @throws AuthorizationFailedException
+     * @throws UserNotFoundException
+     * This api fetches all the question asked by the given user.
+     * Access token and given user id are validated. If any validations fail, one of the above exceptions are thrown.
+     * If no questions have been asked by the user so far, appropriate error message is displayed.
+     * QuestionDetailsResponseTransformer creates response object from the list of question entities fetched from DB.
+     */
     @RequestMapping(method = RequestMethod.GET, path ="/question/all/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<List<QuestionDetailsResponse>> getAllQuestionsByUser(@PathVariable("userId") final String userId, @RequestHeader("accessToken") final String accessToken) throws QuestionNotFoundException,AuthenticationFailedException,AuthorizationFailedException, UserNotFoundException {
         String bearerToken = tokenService.getBearerToken(accessToken);
@@ -64,6 +104,23 @@ public class QuestionController {
         List<QuestionDetailsResponse> questionDetailsResponses = questionDetailsResponseTransformer.transform(questionEntities);
         return new ResponseEntity<List<QuestionDetailsResponse>>(questionDetailsResponses,HttpStatus.OK);
     }
+
+    /**
+     *
+     * @param questionEditRequest
+     * @param questionId
+     * @param accessToken
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws QuestionNotFoundException
+     * @throws AuthenticationFailedException
+     * The question content to be modified is shared via quetionEditRequest.
+     * Access token validations are performed. Question id is also validated in DB.
+     * We also validate if the current user is the owner of the question.
+     * If any of the validations fail, one of the above exceptions are thrown.
+     * Else we update the question using this api.
+     * QuestionEditResponseTransformer creates the response object.
+     */
     @RequestMapping(method = RequestMethod.PUT, path = "/question/edit/{questionId}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionEditResponse> updateQuestion(final QuestionEditRequest questionEditRequest, @PathVariable("questionId") final String questionId, @RequestHeader("accessToken") final String accessToken) throws AuthorizationFailedException, QuestionNotFoundException,AuthenticationFailedException {
         String bearerToken = tokenService.getBearerToken(accessToken);
@@ -73,6 +130,21 @@ public class QuestionController {
         return new ResponseEntity<QuestionEditResponse>(questionEditResponse,HttpStatus.OK);
     }
 
+    /**
+     *
+     * @param questionId
+     * @param accessToken
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws AuthenticationFailedException
+     * @throws QuestionNotFoundException
+     * This api deletes the question with the given question id.
+     * Access token validations are performed. Question id is also validated in DB.
+     * We also validate if the current user is the owner of the question.
+     * If any of the validations fail, one of the above exceptions are thrown.
+     * Else we delete the question using this api.
+     * QuestionDeleteResponseTransformer creates the response object.
+     */
     @RequestMapping(method = RequestMethod.DELETE, path = "/question/delete/{questionId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<QuestionDeleteResponse> deleteQuestion(@PathVariable("questionId") final String questionId, @RequestHeader("accessToken") final String accessToken) throws AuthorizationFailedException, AuthenticationFailedException, QuestionNotFoundException {
         String bearerToken = tokenService.getBearerToken(accessToken);
